064f5575771f2a64c56d794858a2db43a50d5641
Nikita Rai <rainikita595@gmail.com>
2021-08-15 23:42:48 +0530

    [Nikita Rai] Add: Py test for happy test ehn records are matched

---
+++ test_pytesting.py
@@ -0,0 +1,31 @@
+'''
+@Author: Nikita Rai
+@Date: 2021-08-13 8:00:30
+@Last Modified by: Nikita Rai
+@Last Modified time: 2021-08-15 11:00:30
+@Title : pytesting for state code new csv file
+'''
+import pytest
+import os
+import Indian_state
+
+content_SCN, header_SCN = Indian_state.State_Code_new_Analyser.SCNiterator(os.getenv('SCN'))
+record = Indian_state.Indian_state_information.records(content_SCN)
+
+def test_Happy_test_case_SCN(): 
+        '''
+        Description:
+            this is a function which is use to check the number of records are matched or not 
+        Parameter:
+            none
+        Return:
+            none
+        '''
+        assert record== 29
+
+
+
+
+
+       
+        
